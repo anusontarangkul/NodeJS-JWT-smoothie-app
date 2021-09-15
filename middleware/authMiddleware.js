747a8c7dcdbfe9c4d4c233ae72d4ab6a7fe379fb
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-09-15 00:39:23 -0700

    conditional render login and name

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-
+const User = require("../models/User");
 const jwt = require('jsonwebtoken');
 
 const requireAuth = (req, res, next) => {
@@ -20,4 +20,26 @@ const requireAuth = (req, res, next) => {
     }
 };
 
-module.exports = { requireAuth };
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id)
+                res.locals.user = user;
+                next();
+            }
+        });
+
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser };
